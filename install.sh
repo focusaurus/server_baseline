52b00f293a066ba833c256f91af04bad47ce551f
Peter Lyons <pete@peterlyons.com>
2011-12-12 08:35:16 -0700

    apply security updates automatically once a week

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd "$(dirname ${0})"
+DIR="$(pwd)"
+link() {
+    FILE_PATH="${1}"
+    LINK_PATH="${2}"
+    ln -nsf "${FILE_PATH}" "${LINK_PATH}"
+}
+
+for FILE in $(find "./os_files" -type f | cut -d / -f 3-); do
+    echo link "${DIR}/os_files/${FILE}" "/${FILE}"
+done
